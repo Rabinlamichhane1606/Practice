2f371e1f6e8160cd6dcee73abaa38c03f5da7d37
unknown <crestasom@gmail.com>
2018-02-15 11:47:33 +0545

    Spring Configuration completed

---
+++ src/main/java/com/springDemo/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.springDemo.controller;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/hello")
+	public String hello(HttpServletRequest request, HttpServletResponse response) {
+		System.out.println("Here");
+		/*ModelAndView mv=new ModelAndView();
+		mv.setViewName("hello-world");*/
+		return "hello-world.jsp";
+	}
+}
---
+++ src/main/webapp/hello-world.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World from new page!</h2>
+
+</body>
+</html>
